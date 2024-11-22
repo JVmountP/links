b4e90b1869c235f2474d7bdf9308e531ce29e52d
JVmountP <joseph.viernes@georgebrown.ca>
2024-11-22 16:26:13 -0500

    Update sample queries lab12.sql

--- docs/sample queries lab12.sql
+++ docs/sample queries lab12.sql
@@ -1,1 +1,17 @@
+SELECT course_id
+FROM assignments
 
+
+SELECT title, course_id, due_date 
+FROM assignments
+WHERE status = 'In Progress';
+
+
+SELECT course_id, COUNT(*) AS total_assignments
+FROM assignments
+GROUP BY course_id;
+
+
+SELECT c.course_name, a.title, a.status, a.due_date
+FROM courses c
+JOIN assignments a ON c.course_id = a.course_id
